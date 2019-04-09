c5a0e0b275244dbe462adcbbea859994c1bf648e
andriitara <33707313+andriitara@users.noreply.github.com>
2019-04-09 14:03:27 -0700

    compileSdk 28 and androidX migration (#6)
    * Bumped the compileSdk version from 27 to 28
    * Removed depreciating support.vX libraries and added references to same classes from androidX

--- demo/src/com/mobeta/android/demodslv/DSLVFragment.java
+++ demo/src/com/mobeta/android/demodslv/DSLVFragment.java
@@ -2,7 +2,7 @@ package com.mobeta.android.demodslv;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import androidx.fragment.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- library/src/com/mobeta/android/dslv/DragSortCursorAdapter.java
+++ library/src/com/mobeta/android/dslv/DragSortCursorAdapter.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.support.v4.widget.CursorAdapter;
+import androidx.cursoradapter.widget.CursorAdapter;
 import android.util.SparseIntArray;
 import android.view.View;
 import android.view.ViewGroup;
